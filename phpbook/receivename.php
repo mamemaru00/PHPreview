c049a8d79dfbcecfd1ec0499711c571bf21f61f4
mamemaru00 <k.shouhei0711@gmail.com>
2023-04-10 23:42:39 +0900

    基礎プログラム

---
+++ phpbook/receivename.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+   <p>
+    <?php 
+        print ($_POST['namae']);
+    ?>
+    さん、こんにちは
+   </p>
+</body>
+
+</html>
